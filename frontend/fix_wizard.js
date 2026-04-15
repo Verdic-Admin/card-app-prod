cdf504b8922b9685bd1121255323ca3257ac8ccb
Logicinthegap <logicinthegap@gmail.com>
2026-04-15 19:41:47 -0400

    chore: remove proprietary files and internal docs from public template

--- frontend/fix_wizard.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/components/admin/BulkIngestionWizard.tsx', 'utf8');
-if (!c.includes("'use client'")) {
-    c = "'use client'\n" + c;
-}
-c = c.replace('</div>v>', '</div>');
-fs.writeFileSync('src/components/admin/BulkIngestionWizard.tsx', c);
-console.log('Fixed Wizard');
